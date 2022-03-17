9fc2614b438fe3fc603a6798b62ed20f1dcdd898
Dayse Oliveira <dayseoliveira5@gmail.com>
2022-03-16 21:07:07 -0300

    first commit

---
+++ projeto-pokedex-mapadev-week-inicial/src/js/index.js
@@ -0,0 +1,14 @@
+/*
+quando clicar no pokemón da listagem temos que esconder cartão do pokemon
+
+*/
+
+const listaSelecaoPokemons = document.querySelectorAll ('.pokemon')
+
+const pokemonsCard  = document.querySelectorAll ('.cartao-pokemon')
+
+listaSelecaoPokemons.forEach( pokemon => {
+    pokemon.addEventListener ('click', () => {
+        
+    })
+}
